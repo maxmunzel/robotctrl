f1e20865df870ad0be91ddd76c03ed096451a043
Max Nagy <maxmunzel@me.com>
2024-01-08 11:07:40 +0100

    Fix Redis Host

--- redis2polymetis.py
+++ redis2polymetis.py
@@ -3,40 +3,43 @@ import time
 import torch
 from polymetis import RobotInterface
 
-def main(z_height: float = .3, redis_host: str = "localhost", polymetis_ip: str = "10.10.10.210"):
-    r= redis.Redis(decode_responses=True)
 
-    stream_name = 'cart_cmd'
+def main(
+    z_height: float = 0.3,
+    redis_host: str = "localhost",
+    polymetis_ip: str = "10.10.10.210",
+):
+    r = redis.Redis(redis_host, decode_responses=True)
 
-    last_id = '$'
-    
-    robot = RobotInterface(
-            ip_address=polymetis_ip
-            )
-    
+    stream_name = "cart_cmd"
+
+    last_id = "$"
+
+    robot = RobotInterface(ip_address=polymetis_ip)
+
     robot.go_home()
     robot.start_cartesian_impedance()
-    
+
     while not r.xread({stream_name: last_id}, block=1000, count=1):
         print("Waiting for control signals")
-        
+
     try:
         while True:
             messages = r.xread({stream_name: last_id}, block=1000, count=1)[0][1]
 
             if messages:
-                message_id, payload = messages[-1] 
+                message_id, payload = messages[-1]
                 last_id = message_id  # Update last_id for the next iteration
 
                 # Calculate latency
-                message_time = float(message_id.split('-')[0])
+                message_time = float(message_id.split("-")[0])
                 current_time = float(time.time() * 1000)
                 latency = current_time - message_time
                 x = float(payload["x"])
                 y = float(payload["y"])
 
                 goal_pos = torch.Tensor([x, y, z_height])
-                goal_quat = torch.Tensor([0,0,1,0])
+                goal_quat = torch.Tensor([0, 0, 1, 0])
                 robot.update_desired_ee_pose(goal_pos, goal_quat)
 
                 print(f"x: {x:.2f}, y: {y:.2f}, Latency: {latency:.2f} ms")
@@ -47,6 +50,8 @@ def main(z_height: float = .3, redis_host: str = "localhost", polymetis_ip: str 
     finally:
         robot.go_home()
 
+
 if __name__ == "__main__":
     import typer
+
     typer.run(main)
