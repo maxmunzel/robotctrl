136afb924a95bd77997264225db30e0dbc1b4285
Max Nagy <maxmunzel@me.com>
2024-02-03 16:25:42 +0100

    Road to Rollouts
    * Don't skip over commands
    * Don't skip the first message
    * Remove Timeout
    * Add Reset Mechanism

--- redis2polymetis.py
+++ redis2polymetis.py
@@ -2,6 +2,8 @@ import redis
 import time
 import torch
 from polymetis import RobotInterface
+import json
+import numpy as np
 
 
 def main(
@@ -11,46 +13,76 @@ def main(
 ):
     r = redis.Redis(redis_host, decode_responses=True)
 
-    stream_name = "cart_cmd"
+    cmd_stream = "cart_cmd"
+    obs_stream = "real_robot_obs"
 
     last_id = "$"
 
     robot = RobotInterface(ip_address=polymetis_ip)
 
     robot.go_home()
+    print("Ready.")
 
-    while not r.xread({stream_name: last_id}, block=1000, count=1):
-        print("Waiting for control signals")
+    # while not r.xread({cmd_stream: last_id}, block=1000, count=1):
+    #    print("Waiting for control signals")
 
+    goal_quat = torch.Tensor([0.92387975, 0.3826829, 0.0, 0.0])
     first_cmd = True
     try:
         while True:
-            messages = r.xread({stream_name: last_id}, block=1000, count=1)
+            messages = r.xread({cmd_stream: last_id}, block=0, count=1)
 
             if messages:
                 message_id, payload = messages[0][1][-1]
                 # for fast sources, simply waiting for a new message is better
-                last_id = "$"  # message_id  # Update last_id for the next iteration
+                last_id = message_id  # Update last_id for the next iteration
+                robot.start_joint_impedance()
+                if payload["cmd"] == "GOTO":
+                    # Calculate latency
+                    message_time = float(message_id.split("-")[0])
+                    current_time = float(time.time() * 1000)
+                    latency = current_time - message_time
+                    x = float(payload["x"])
+                    y = float(payload["y"])
 
-                # Calculate latency
-                message_time = float(message_id.split("-")[0])
-                current_time = float(time.time() * 1000)
-                latency = current_time - message_time
-                x = float(payload["x"])
-                y = float(payload["y"])
+                    goal_pos = torch.Tensor([x, y, z_height])
+                    if first_cmd:
+                        robot.move_to_ee_pose(
+                            position=goal_pos, orientation=goal_quat, time_to_go=2.0
+                        )
+                        robot.start_joint_impedance()
+                        first_cmd = False
+                    else:
+                        robot.update_desired_ee_pose(goal_pos, goal_quat)
 
-                goal_pos = torch.Tensor([x, y, z_height])
-                goal_quat = torch.Tensor([0.92387975, 0.3826829, 0., 0.])
-                if first_cmd:
+                    print(f"x: {x:.2f}, y: {y:.2f}, Latency: {latency:.2f} ms")
+                else:
+                    assert (
+                        payload["cmd"] == "RESET"
+                    ), f"Unknown Command: {payload['cmd']}"
+                    if first_cmd:
+                        robot.start_joint_impedance()
+                        first_cmd = False
+                    print("RESET: Please move box and press enter to confirm.")
+                    input()
+                    res = r.xrevrange("box_tracking", "+", "-", count=1)
+                    assert res
+                    _, payload = res[0]  # type: ignore
+                    transform = json.loads(payload["transform"])
+                    transform = np.array(transform).reshape(4, 4)
+                    x, y = transform[:2, 3].flatten()
+                    goal_pos = torch.Tensor([x, y, 0.45])
                     robot.move_to_ee_pose(
                         position=goal_pos, orientation=goal_quat, time_to_go=2.0
                     )
-                    robot.start_joint_impedance()
-                    first_cmd = False
-                else:
-                    robot.update_desired_ee_pose(goal_pos, goal_quat)
-
-                print(f"x: {x:.2f}, y: {y:.2f}, Latency: {latency:.2f} ms")
+                    print("Press Enter to confirm position")
+                    input()
+                    print(f"x: {x:.2f}, y: {y:.2f}, RESET DONE")
+                    goal_pos = torch.Tensor([x, y, z_height])
+                    robot.move_to_ee_pose(
+                        position=goal_pos, orientation=goal_quat, time_to_go=2.0
+                    )
+                    r.xadd(obs_stream, {"reset": 1, "x": x, "y": y})
 
             else:
                 print("No message received for 1000ms. Exiting.")
